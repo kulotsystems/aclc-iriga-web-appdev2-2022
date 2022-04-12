e132c31bf6b5983bda7a6d2628433bb7bb30c5da
Sarah Mae Tuyay <sarahmaetuyay00@gmail.com>
2022-04-12 14:07:19 +0800

    Create sarahmae.php

---
+++ public/members/data/sarahmae.php
@@ -0,0 +1,18 @@
+<?php
+
+    array_push($members, [
+
+        'first_name'   => 'Sarah Mae',
+        'middle_name'  => 'Mujal',
+        'last_name'    => 'Tuyay',
+        'name_suffix'  => '',
+        'birth_date'   => '1999 05 03',
+        'gender'       => 'Female',
+        'github' => [
+            'username' => 'sarahmeeyy',
+            'avatar'   => 'https://avatars.githubusercontent.com/u/91459915?s=96&v=4'
+        ]
+
+    ]);
+
+?>
