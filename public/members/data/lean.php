d7d86cacd86c918dd7bb49c59fd73cc868e80535
Lean Joshua Sarmiento <sarmientoleanjoshua24@gmail.com>
2022-04-12 20:00:02 -0700

    Create lean.php

---
+++ public/members/data/lean.php
@@ -0,0 +1,18 @@
+<?php
+
+    array_push($members, [
+
+        'first_name'   => 'Lean Joshua',
+        'middle_name'  => 'Tercenio',
+        'last_name'    => 'Sarmiento',
+        'name_suffix'  => '',
+        'birth_date'   => '2001-04-24',
+        'gender'       => 'Male',
+        'github' => [
+            'username' => 'ljsarmiento',
+            'avatar'   => 'https://avatars.githubusercontent.com/u/103472931?v=4'
+        ]
+
+    ]);
+
+?>
