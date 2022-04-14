8fd32fb7b886b0190aedfee9eaf12e36df82683f
Jeffrey Lagdaan <lagdaanjeffrey@gmail.com>
2022-04-14 10:48:22 +0800

    Create jeffrey.php

---
+++ public/members/data/jeffrey.php
@@ -0,0 +1,18 @@
+<?php
+
+    array_push($members, [
+
+        'first_name'   => 'Jeffrey',
+        'middle_name'  => 'Villanueva',
+        'last_name'    => 'Lagdaan',
+        'name_suffix'  => '',
+        'birth_date'   => '2001,07,01',
+        'gender'       => 'Male',
+        'github' => [
+            'username' => 'jeffrey200107',
+            'avatar'   => 'https://avatars.githubusercontent.com/u/103405944?v=4'
+        ]
+
+    ]);
+
+?>
