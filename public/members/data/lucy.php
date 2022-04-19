4035a8d91b3774924cff6da21a354a17f7aaee2f
Lucy Magistrado <103977056+lucymagis14@users.noreply.github.com>
2022-04-19 11:01:55 +0800

    Create lucy.php

---
+++ public/members/data/lucy.php
@@ -0,0 +1,19 @@
+<?php
+
+    array_push($members, [
+
+        'first_name'   => 'Lucy',
+        'middle_name'  => 'Estela',
+        'last_name'    => 'Magistrado',
+        'name_suffix'  => '',
+        'birth_date'   => '2000-10-1',
+        'gender'       => 'Female',
+        'github' => 
+        [
+            'username' => 'lucymagis',
+            'avatar'   => 'https://avatars.githubusercontent.com/u/103977056?v=4'
+        ]
+
+    ]);
+
+?>
