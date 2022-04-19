1fc1997a427444112110f518c5b86de0ef737b37
Gella Mae Ongog <eliyanahongog@gmail.com>
2022-04-19 13:42:58 -0700

    Create Gella.php

---
+++ public/members/data/Gella.php
@@ -0,0 +1,16 @@
+<?php
+
+    array_push($members, [
+
+        'first_name'   => 'Gella Mae',
+        'middle_name'  => 'Mesuga',
+        'last_name'    => 'Ongog',
+        'name_suffix'  => '',
+        'birth_date'   => '1999-07-23',
+        'gender'       => 'Female',
+        'github' => [
+            'username' => 'Gellayy23',
+            'avatar'   => 'https://avatars.githubusercontent.com/u/103555642?v=4'
+        ]
+
+    ]);
