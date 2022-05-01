749d1faa4878b5d46e2e4498053c5c91652761d0
Ron Andrew S Villaluz <ronandrewvillaluz@gmail.com>
2022-05-01 23:22:56 +0100

    Create Ron.php

---
+++ public/members/data/Ron.php
@@ -0,0 +1,18 @@
+<?php
+
+    array_push($members, [
+
+        'first_name'   => 'Ron andrew',
+        'middle_name'  => 'Sesno',
+        'last_name'    => 'Villaluz',
+        'name_suffix'  => '',
+        'birth_date'   => '1998-03-29',
+        'gender'       => 'Male',
+        'github' => [
+            'username' => 'Villaluz',
+            'avatar'   => 'https://avatars.githubusercontent.com/u/103405962?s=400&u=f14469f6581d4b1a5bcedef552eb01de35cd1644&v=4'
+        ]
+
+    ]);
+
+?>
