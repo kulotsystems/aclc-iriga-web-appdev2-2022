7c0939f7044af2217971081a7d96d5099519b440
Johnbernbolo <johnbernbolo0814@gmail.com>
2022-05-23 19:58:54 -0700

    Create johnbern.php

---
+++ public/members/data/johnbern.php
@@ -0,0 +1,18 @@
+<?php
+
+    array_push($members, [
+
+        'first_name'   => 'John Bern',
+        'middle_name'  => 'Peñalosa',
+        'last_name'    => 'Bolo',
+        'name_suffix'  => '',
+        'birth_date'   => '2000-10-8',
+        'gender'       => 'Male',
+        'github' => [
+            'username' => 'johnbernbolo',
+            'avatar'   => 'https://avatars.githubusercontent.com/u/105410210?v=4'
+        ]
+
+    ]);
+
+?>
