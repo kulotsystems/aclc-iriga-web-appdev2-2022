0b6eced11da1d32cc77ec1f08f5cd006cbd7282e
Vhanmartinez <martinezvhanwishley@gmail.com>
2022-05-31 19:13:23 +0800

    data(vhan)

---
+++ public/members/data/vhan.php
@@ -0,0 +1,18 @@
+<?php
+
+    array_push($members, [
+
+        'first_name'   => 'Vhan Wishley',
+        'middle_name'  => 'Togores',
+        'last_name'    => 'Martinez',
+        'name_suffix'  => '',
+        'birth_date'   => '1998-11-15',
+        'gender'       => 'Male',
+        'github' => [
+            'username' => 'vhanmartinez',
+            'avatar'   => 'https://avatars.githubusercontent.com/u/90486398?s=400&u=467b2cd832d865a3c12cc37194c5bb35905c3075&v=4'
+        ]
+
+    ]);
+
+?>
