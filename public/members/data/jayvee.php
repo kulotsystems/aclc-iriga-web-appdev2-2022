54c7e60d1571c42544a4a3e5f0e584c17144b8b2
Jvlavapie30 <jvlavapie30@gmail.com>
2022-06-18 01:28:16 -0700

    Jayvee lavapie

---
+++ public/members/data/jayvee.php
@@ -0,0 +1,18 @@
+<?php
+
+    array_push($members, [
+
+        'first_name'   => 'Jayvee',
+        'middle_name'  => 'Casyao',
+        'last_name'    => 'Lavapie',
+        'name_suffix'  => '',
+        'birth_date'   => '2000-11-07',
+        'gender'       => 'Male',
+        'github' => [
+            'username' => 'Jvlavapie30',
+            'avatar'   => 'https://avatars.githubusercontent.com/u/104925477?v=4'
+        ]
+
+    ]);
+
+?>
